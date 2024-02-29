55a7e6c564ac3b3a9dd37f20ee31efa605229244
linuxul <jsseo@eflash.co.kr>
2024-02-29 13:45:05 +0900

    [feat] Do it! 리액트 네이티브 앱 프로그래밍 02-1

---
+++ ch021/.prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false
+};
