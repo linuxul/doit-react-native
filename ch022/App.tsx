3d6290879093ba5a0a07e6b67eef0250e0cb1fca
linuxul <jsseo@eflash.co.kr>
2024-03-01 10:16:06 +0900

    [feat] ch022 실습

--- ch022/App.tsx
+++ ch022/App.tsx
@@ -56,44 +56,95 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const children = new Array(100)
+    .fill(null)
+    .map((notUsed, index) => <Text>Hello world! {index}</Text>);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  return <SafeAreaView>{children}</SafeAreaView>;
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  // const children = [
+  //   <Text>Hello world! 1</Text>,
+  //   <Text>Hello world! 2</Text>,
+  //   <Text>Hello world! 3</Text>,
+  // ];
+
+  // // 배열은 반드시 부모 컴포턴느의 작식 컴포넌트 형태로 작성해야 합니다.
+  // return <SafeAreaView>{children}</SafeAreaView>;
+
+  // const isLoading = false;
+
+  // if (isLoading) {
+  //   return (
+  //     <SafeAreaView>
+  //       <Text>Loading...</Text>
+  //     </SafeAreaView>
+  //   );
+  // } else {
+  //   return (
+  //     <SafeAreaView>
+  //       <Text>Hello world JSX!</Text>
+  //     </SafeAreaView>
+  //   );
+  // }
+
+  // return (
+  //   <SafeAreaView>
+  //     <Text>Hello JSX world!</Text>
+  //   </SafeAreaView>
+  // );
+
+  // const virtualDOM = (
+  //   <SafeAreaView>
+  //     <Text>Hello JSX world!</Text>
+  //   </SafeAreaView>
+  // );
+
+  // return virtualDOM;
+
+  // return (
+  //   <SafeAreaView>
+  //     <Text>Hello JSX world!</Text>
+  //   </SafeAreaView>
+  // );
+
+  // const isDarkMode = useColorScheme() === 'dark';
+
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
+
+  // return (
+  //   <SafeAreaView style={backgroundStyle}>
+  //     <StatusBar
+  //       barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+  //       backgroundColor={backgroundStyle.backgroundColor}
+  //     />
+  //     <ScrollView
+  //       contentInsetAdjustmentBehavior="automatic"
+  //       style={backgroundStyle}>
+  //       <Header />
+  //       <View
+  //         style={{
+  //           backgroundColor: isDarkMode ? Colors.black : Colors.white,
+  //         }}>
+  //         <Section title="Step One">
+  //           Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+  //           screen and then come back to see your edits.
+  //         </Section>
+  //         <Section title="See Your Changes">
+  //           <ReloadInstructions />
+  //         </Section>
+  //         <Section title="Debug">
+  //           <DebugInstructions />
+  //         </Section>
+  //         <Section title="Learn More">
+  //           Read the docs to discover what to do next:
+  //         </Section>
+  //         <LearnMoreLinks />
+  //       </View>
+  //     </ScrollView>
+  //   </SafeAreaView>
+  // );
 }
 
 const styles = StyleSheet.create({
